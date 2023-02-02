3cb53d35a309e235515be8d955653024236523d0
James <jamesf@auscert.org>
2023-02-02 16:22:08 +1100

    add bash pollkb.sh

---
+++ pollkb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+vendor_product="hiddev96"
+
+while read -r line; do
+	if [[ $line =~ $vendor_product ]]; then
+		if [[ $line =~ hidraw([0-9]+): ]]; then
+			hidraw_device=${BASH_REMATCH[1]}
+			echo "hidraw device found at number $hidraw_device"
+
+			device="/dev/hidraw$hidraw_device"
+			duration=1
+
+			(cat $device &)
+			pid=$!
+
+			echo "Reading from $device for $duration seconds..."
+			sleep $duration
+
+			echo "Killing cat... REOWW"
+			kill $pid
+			break
+		else
+			echo "Could not extract hidraw device number from line."
+			break
+		fi
+	fi
+done < <(dmesg)
